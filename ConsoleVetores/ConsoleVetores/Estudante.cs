79a1d3d0583a5e9a111d2b0821cb3537f276898a
ClaraViieira <mariahclara_@hotmail.com>
2022-08-18 11:49:08 -0300

    Projeto Console Vetores com classe e propriedades

---
+++ ConsoleVetores/ConsoleVetores/Estudante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleVetores {
+    class Estudante {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Estudante(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString() {
+            return Nome + ", " + Email;
+        }
+    }
+}
