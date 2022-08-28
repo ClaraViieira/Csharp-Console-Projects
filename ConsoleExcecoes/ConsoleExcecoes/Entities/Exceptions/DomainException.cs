631e291b061106f34f48e9273104be334483cf49
ClaraViieira <mariahclara_@hotmail.com>
2022-08-28 18:19:12 -0300

    Consoles usando tratamento de exceções

---
+++ ConsoleExcecoes/ConsoleExcecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleExcecoes.Entities.Exceptions {
+    class DomainException : ApplicationException {
+        public DomainException(string message) : base(message) {
+        }
+    }
+}
